44498ebf77af7a008397c9ebc54786afbebf4089
NicolasSampai <araujo.sampaio.nicolas@gmail.com>
2026-04-21 19:27:26 -0400

    fix(prisma): implement seed, fix course-student foreign key issue, and update client provider/imports

--- backend/src/infrastructure/database/prisma.ts
+++ backend/src/infrastructure/database/prisma.ts
@@ -1,8 +1,9 @@
+import "dotenv/config";
 import { PrismaClient } from "../../../prisma/src/infrastructure/database/generated/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL as string,
+  connectionString: process.env.DATABASE_URL,
 });
 
 const prisma = new PrismaClient({ adapter });
