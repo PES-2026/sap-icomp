ace2775033f95e1d2867aa67615f0b8fe71d1b07
Thiago Vinicius <thiago7313vini@gmail.com>
2026-04-21 22:08:28 -0400

    fix: adding cors free

--- backend/src/presentation/server.ts
+++ backend/src/presentation/server.ts
@@ -19,22 +19,7 @@ const allowedOrigins = [
   `http://${process.env.FRONTEND_HOST}:${process.env.FRONTEND_PORT}`,
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"), false);
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  }),
-);
+app.use(cors());
 
 app.options("*", cors());
 
