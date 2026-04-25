248911d8ad95a6a2ef0e793a0517048ff04f2f6b
Thiago Vinicius Costa Guimaraes <thiago7313vini@gmail.com>
2026-04-24 23:02:06 -0400

    feat(task-101): adding valueObjects from attendance

---
+++ backend/src/domain/enums/attendance/attendanceType.enum.ts
@@ -0,0 +1,8 @@
+export enum AttendanceType {
+  Learning = "LEARNING",
+  SocioeconomicVulnerability = "SOCIOECONOMIC_VULNERABILITY",
+  Emotional = "EMOTIONAL",
+  Deficiency = "DEFICIENCY",
+  AcademicOrientation = "ACADEMIC_ORIENTATION",
+  Others = "OTHERS",
+}
