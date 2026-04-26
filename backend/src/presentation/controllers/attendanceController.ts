3e9f971ac76ce914c441c76d6c9cc453ea198e30
Thiago Vinicius Costa Guimaraes <thiago7313vini@gmail.com>
2026-04-26 17:58:32 -0400

    feat(task-105): adding list attendances controller and route

--- backend/src/presentation/controllers/attendanceController.ts
+++ backend/src/presentation/controllers/attendanceController.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from "express";
 import { CreateAttendanceDTO } from "../../application/dtos/attendance/createAttendance.dto";
 import { CreateAttendance } from "../../application/use-cases/attendance/createAttendance";
+import { ListAttendances } from "../../application/use-cases/attendance/listAttendances";
+import { ListAttendanceDTO } from "../../application/dtos/attendance/listAttendance.dto";
 
 export class AttendanceController {
-  constructor(private createAttendance: CreateAttendance) {}
+  constructor(
+    private createAttendance: CreateAttendance,
+    private listAttendances: ListAttendances,
+  ) {}
 
   async create(req: Request, res: Response): Promise<void> {
     try {
@@ -19,6 +24,23 @@ export class AttendanceController {
 
       res.status(500).json({
         message: `Internal server error to: ${AttendanceController.name}:${this.create.name}`,
+      });
+    }
+  }
+
+  async list(req: Request, res: Response): Promise<void> {
+    try {
+      const dto = ListAttendanceDTO.create(req.query);
+      const result = await this.listAttendances.execute(dto);
+      res.status(200).json(result);
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(400).json({ message: error.message });
+        return;
+      }
+
+      res.status(500).json({
+        message: `Internal server error to: ${AttendanceController.name}:${this.list.name}`,
       });
     }
   }
