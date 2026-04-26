2eaa2266ff6156434004c4e49961f1b0f541db2b
Thiago Vinicius Costa Guimaraes <thiago7313vini@gmail.com>
2026-04-26 17:54:46 -0400

    feat(task-105): adding number field validator

--- backend/src/domain/utils/validation.utils.ts
+++ backend/src/domain/utils/validation.utils.ts
@@ -24,3 +24,12 @@ export function validateDateField(value: unknown, fieldName: string): Date {
 
   return parsedDate;
 }
+
+export function validateNumberField(value: unknown, fieldName: string): number {
+  if (typeof value !== "number" || isNaN(value)) {
+    throw new Error(
+      `${fieldName} is required and must a number. Please verify it!`,
+    );
+  }
+  return value as number;
+}
