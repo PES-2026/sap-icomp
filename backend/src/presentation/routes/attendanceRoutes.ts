4b30d10a4791c13b0999e4a2cbfd42948bc35c65
Thiago Vinicius Costa Guimaraes <thiago7313vini@gmail.com>
2026-04-26 17:58:32 -0400

    feat(task-105): adding list attendances controller and route

--- backend/src/presentation/routes/attendanceRoutes.ts
+++ backend/src/presentation/routes/attendanceRoutes.ts
@@ -4,5 +4,6 @@ import { AttendanceController } from "../controllers/attendanceController";
 export function attendanceRoutes(controller: AttendanceController): Router {
   const router = Router();
   router.post("/attendances", controller.create);
+  router.get("/attendances", controller.list);
   return router;
 }
