17d102ff10aa4c7052fe14ec5c01061afa3df4ca
Thiago Vinicius Costa Guimaraes <thiago7313vini@gmail.com>
2026-04-26 23:08:41 -0400

    refactor(task-116): changing dtos to common

--- backend/src/application/dtos/shared/paginationDto.ts
+++ backend/src/application/dtos/shared/paginationDto.ts
@@ -4,3 +4,10 @@ export interface PaginatedResult<T> {
   currentPage: number;
   items: T[];
 }
+
+export type PaginatedRequest<K extends string, V> = {
+  page: number;
+  limit: number;
+} & {
+  [key in K]: V;
+};
