45e1c985e7b13fabf014e6162e47c05b76f11659
Thiago Vinicius Costa Guimaraes <thiago7313vini@gmail.com>
2026-04-28 00:47:56 -0400

    fix(task-102): optional fields to update in use case and return the response

--- backend/src/application/use-cases/attendance/updateAttendance.ts
+++ backend/src/application/use-cases/attendance/updateAttendance.ts
@@ -3,12 +3,15 @@ import { AttendanceTypeVO } from "../../../domain/valueObjects/attendance/attend
 import { DemandVO } from "../../../domain/valueObjects/attendance/demand";
 import { GeneralObservationsVO } from "../../../domain/valueObjects/attendance/generalObservations";
 import { DateVO } from "../../../domain/valueObjects/shared/date";
-import { UpdateAttendanceDTO } from "../../dtos/attendance/updateAttedance.dto";
+import {
+  UpdateAttendanceDTO,
+  UpdateAttendanceResponse,
+} from "../../dtos/attendance/updateAttedance.dto";
 
 export class UpdateAttendance {
   constructor(private repository: IAttendanceRepository) {}
 
-  async execute(dto: UpdateAttendanceDTO): Promise<void> {
+  async execute(dto: UpdateAttendanceDTO): Promise<UpdateAttendanceResponse> {
     const attendance = await this.repository.findById(dto.id);
 
     if (!attendance) {
@@ -16,12 +19,23 @@ export class UpdateAttendance {
     }
 
     attendance.update(
-      AttendanceTypeVO.create(dto.type),
-      DateVO.create(dto.date),
-      DemandVO.create(dto.demand),
-      GeneralObservationsVO.create(dto.generalObservations),
+      dto.type ? AttendanceTypeVO.create(dto.type) : undefined,
+      dto.date ? DateVO.create(dto.date) : undefined,
+      dto.demand ? DemandVO.create(dto.demand) : undefined,
+      dto.generalObservations
+        ? GeneralObservationsVO.create(dto.generalObservations)
+        : undefined,
     );
 
     await this.repository.update(attendance);
+
+    return {
+      id: attendance.id.value,
+      studentId: attendance.studentId.value,
+      type: attendance.type.value,
+      date: attendance.date.value,
+      demand: attendance.demand.value,
+      generalObservations: attendance.generalObservations?.value ?? "",
+    };
   }
 }
