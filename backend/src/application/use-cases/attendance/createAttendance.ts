87fc17a1b8bbde65f9a1390238b84a1f06ec1442
Thiago Vinicius Costa Guimaraes <thiago7313vini@gmail.com>
2026-04-27 23:32:09 -0400

    fix(task-101): create the attendance and return

--- backend/src/application/use-cases/attendance/createAttendance.ts
+++ backend/src/application/use-cases/attendance/createAttendance.ts
@@ -1,27 +1,31 @@
 import { Attendance } from "../../../domain/entities/attendance";
 import { IAttendanceRepository } from "../../../domain/repositories/attendanceRepository";
-import { AttendanceTypeVO } from "../../../domain/valueObjects/attendance/attendanceType";
-import { DemandVO } from "../../../domain/valueObjects/attendance/demand";
-import { GeneralObservationsVO } from "../../../domain/valueObjects/attendance/generalObservations";
-import { DateVO } from "../../../domain/valueObjects/shared/date";
-import { ExternalIdVO } from "../../../domain/valueObjects/shared/externalId";
-import { StudentId } from "../../../domain/valueObjects/student/studentId";
-import { CreateAttendanceDTO } from "../../dtos/attendance/createAttendance.dto";
+import {
+  CreateAttendanceDTO,
+  CreateAttendanceResponse,
+} from "../../dtos/attendance/createAttendance.dto";
 
 export class CreateAttendance {
   constructor(private repository: IAttendanceRepository) {}
 
-  async execute(input: CreateAttendanceDTO): Promise<Attendance> {
-    const attendance = new Attendance(
-      ExternalIdVO.create(),
-      StudentId.reutilise(input.studentId),
-      DateVO.create(input.date),
-      AttendanceTypeVO.create(input.type),
-      DemandVO.create(input.demand),
-      GeneralObservationsVO.create(input.generalObservations),
+  async execute(dto: CreateAttendanceDTO): Promise<CreateAttendanceResponse> {
+    const attendance = Attendance.create(
+      dto.studentId,
+      dto.date,
+      dto.type,
+      dto.demand,
+      dto.generalObservations ? dto.generalObservations : undefined,
     );
 
     await this.repository.save(attendance);
-    return attendance;
+
+    return {
+      id: attendance.id.value,
+      studentId: attendance.studentId.value,
+      type: attendance.type.value,
+      date: attendance.date.value,
+      demand: attendance.demand.value,
+      generalObservations: attendance.generalObservations?.value ?? "",
+    };
   }
 }
