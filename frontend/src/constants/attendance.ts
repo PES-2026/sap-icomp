08be67de6aff1e0a25caeef6aaf98e3fa3bb55ec
Thiago Vinicius Costa Guimarães <thiago7313vinI@gmail.com>
2026-04-28 15:57:03 -0400

    fix(task-106): fix attendances types (#222)
    * fix(task-106): fix attendances types
    
    * fix(task-106): fix attendances types

--- frontend/src/constants/attendance.ts
+++ frontend/src/constants/attendance.ts
@@ -1,7 +1,13 @@
-export const AttendanceTypes = [
-  "Atendimento",
-  "Primeiro Contato / Atendimento",
-  "Alinhamento",
-  "Orientação",
-  "Outro",
+import { Option } from "@/components/select-input/FilterSelect";
+
+export const AttendanceTypes: Option[] = [
+  { value: "Learning", label: "Aprendizado" },
+  {
+    value: "SocioeconomicVulnerability",
+    label: "Vulnerabilidade Socioeconômica",
+  },
+  { value: "Emotional", label: "Emocional" },
+  { value: "Deficiency", label: "Deficiência" },
+  { value: "AcademicOrientation", label: "Orientação Acadêmica" },
+  { value: "Others", label: "Outros" },
 ];
