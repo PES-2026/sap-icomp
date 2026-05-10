69b850bc97bb57eb0c7f42c8eb2784be08440761
Marco Antonio <dev.mmarco@gmail.com>
2026-05-10 18:35:38 -0400

    feat: integration with the settings page

--- frontend/src/app/admin/settings/page.tsx
+++ frontend/src/app/admin/settings/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import AttendanceTypeSection from "@/features/attendance-types/components/AttendanceTypeSection";
+import DiagnosticSection from "@/features/diagnostics/components/DiagnosticSection";
 
 export default function SettingsPage() {
   return (
     <div className="flex flex-col min-h-[calc(100vh-100px)] overflow-auto p-4 md:p-8 gap-6 font-sans">
       <AttendanceTypeSection />
+      <DiagnosticSection />
     </div>
   );
 }
