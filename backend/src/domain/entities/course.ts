5e97e3eadb6f61832b7174bc4f7d357a9a0529e6
NicolasSampai <araujo.sampaio.nicolas@gmail.com>
2026-05-11 00:21:23 -0400

    Course entity

--- backend/src/domain/entities/course.ts
+++ backend/src/domain/entities/course.ts
@@ -1,35 +1,16 @@
-import { CourseName } from "../valueObjects/course/courseName";
 import { Acronym } from "../valueObjects/course/acronym";
+import { CourseName } from "../valueObjects/course/courseName";
 import { ProfessorId } from "../valueObjects/professor/professorId";
-import { ExternalIdVO } from "../valueObjects/shared/externalId";
 export class Course {
   constructor(
-    readonly externalId: ExternalIdVO,
-    readonly name: CourseName,
-    readonly acronym: Acronym,
-    readonly coordenatorId?: ProfessorId,
+    private readonly name: CourseName,
+    private readonly acronym: Acronym,
+    private readonly coordenatorId?: ProfessorId,
   ) {}
   static create(name: string, acronym: string, coordenatorId?: string): Course {
-    const externalId = ExternalIdVO.create();
     const courseName = CourseName.create(name);
     const c_acronym = Acronym.create(acronym);
-    const c_coordenatorId = coordenatorId
-      ? ProfessorId.reutilise(coordenatorId)
-      : undefined;
-    return new Course(externalId, courseName, c_acronym, c_coordenatorId);
-  }
-  static update(
-    externalId: string,
-    name: string,
-    acronym: string,
-    coordenatorId?: string,
-  ): Course {
-    const courseExternalId = ExternalIdVO.from(externalId);
-    const courseName = CourseName.create(name);
-    const c_acronym = Acronym.create(acronym);
-    const c_coordenatorId = coordenatorId
-      ? ProfessorId.reutilise(coordenatorId)
-      : undefined;
-    return new Course(courseExternalId, courseName, c_acronym, c_coordenatorId);
+    const c_coordenatorId = coordenatorId ? ProfessorId.reutilise(coordenatorId) : undefined;
+    return new Course(courseName, c_acronym, c_coordenatorId);
   }
 }
--- backend/src/domain/repositories/courseRepository.ts
+++ backend/src/domain/repositories/courseRepository.ts
@@ -1,10 +1,4 @@
 import { Course } from "../entities/course";
-import {
-  ListCourseRequest,
-  ListCourseResponse,
-} from "../../application/dtos/course/listCourse.dto";
-export interface ICourseRepository {
+export interface courseRository {
   save(course: Course): Promise<void>;
-  update(course: Course): Promise<void>;
-  findAll(params: ListCourseRequest): Promise<ListCourseResponse>;
 }
