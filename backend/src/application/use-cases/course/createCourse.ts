a98c2cf4094e7aeb424f0a62daf225ac4e18bc74
NicolasSampai <araujo.sampaio.nicolas@gmail.com>
2026-05-11 12:12:39 -0400

    create course use-cases

---
+++ backend/src/application/use-cases/course/createCourse.ts
@@ -0,0 +1,19 @@
+import { Course } from "../../../domain/entities/course";
+import {
+  CreateCourseDTO,
+  CreateCourseResponse,
+} from "../../dtos/course/createCourse.dto";
+import { ICourseRepository } from "../../../domain/repositories/courseRepository";
+
+export class CreateCourse {
+  constructor(private repository: ICourseRepository) {}
+  async execute(dto: CreateCourseDTO): Promise<CreateCourseResponse> {
+    const course = Course.create(dto.name, dto.acronym, dto.coordinatorId);
+    await this.repository.save(course);
+    return {
+      name: course.name.value,
+      acronym: course.acronym.value,
+      coordinatorId: course.coordenatorId?.value,
+    };
+  }
+}
