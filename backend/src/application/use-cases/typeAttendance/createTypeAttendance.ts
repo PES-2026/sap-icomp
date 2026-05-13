467ebe1886bcaf67a2a47d02ceb1f86277a7cd76
Nicolas <araujo.sampaio.nicolas@gmail.com>
2026-05-13 13:56:22 -0400

    refactor: rename CreateTypeAttendanceDto to CreateTypeAttendanceDTO for consistency

--- backend/src/application/use-cases/typeAttendance/createTypeAttendance.ts
+++ backend/src/application/use-cases/typeAttendance/createTypeAttendance.ts
@@ -1,12 +1,11 @@
-import { reateTypeAttendanceDto } from "../dtos/typeAttendance/createTypeAttendance.dto";
-import { ITypeAttendanceRepository } from "../../domain/repositories/typeAttendanceRepository";
-import { TypeAttendance } from "../../domain/entities/typeAttendance";
-import { CreateTypeAttendanceDto } from "../../dtos/typeAttendance/createTypeAttendance.dto";
+import { CreateTypeAttendanceDTO } from "../../dtos/typeAttendance/createTypeAttendance.dto";
+import { ITypeAttendanceRepository } from "../../../domain/repositories/typeAttendanceRepository";
+import { TypeAttendance } from "../../../domain/entities/typeAttendance";
 
 export class CreateTypeAttendance {
   constructor(private typeAttendanceRepository: ITypeAttendanceRepository) {}
 
-  async execute(dto: CreateTypeAttendanceDto): Promise<TypeAttendance> {
+  async execute(dto: CreateTypeAttendanceDTO): Promise<TypeAttendance> {
     const typeAttendance = TypeAttendance.create(dto);
     await this.typeAttendanceRepository.save(typeAttendance);
     return typeAttendance;
