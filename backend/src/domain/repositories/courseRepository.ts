86d7719dbc9ea39af3ee5204b6b9e093333331b2
Nicolas <araujo.sampaio.nicolas@gmail.com>
2026-05-12 21:10:33 -0400

    Implement findAll method in PrismaCourseRepository for course retrieval with pagination and filtering

--- backend/src/domain/repositories/courseRepository.ts
+++ backend/src/domain/repositories/courseRepository.ts
@@ -1,5 +1,10 @@
 import { Course } from "../entities/course";
+import {
+  ListCourseRequest,
+  ListCourseResponse,
+} from "../../application/dtos/course/listCourse.dto";
 export interface ICourseRepository {
   save(course: Course): Promise<void>;
   update(course: Course): Promise<void>;
+  findAll(params: ListCourseRequest): Promise<ListCourseResponse>;
 }
