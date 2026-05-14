2b0d72cf5815f14d8a3de02c97c4c3404370628e
Thiago Vinicius Costa Guimaraes <thiago7313vini@gmail.com>
2026-05-14 00:06:14 -0400

    feat: implementing middlewares on course routes

--- backend/src/presentation/routes/courseRoutes.ts
+++ backend/src/presentation/routes/courseRoutes.ts
@@ -1,14 +1,23 @@
 import { Router } from "express";
 
+import { CourseByIdDTO } from "@application/dtos/course/courseByIdDto";
+import { CreateCourseDTO } from "@application/dtos/course/createCourseDto";
+import { ListCourseDTO } from "@application/dtos/course/listCourseDto";
+import { RemoveCourseDTO } from "@application/dtos/course/removeCourseDto";
+import { UpdateCourseDTO } from "@application/dtos/course/updateCourseDto";
+import { validateBody } from "@presentation/middlewares/validateBody";
+import { validateParams } from "@presentation/middlewares/validateParams";
+
 import { CourseController } from "../controllers/courseController";
 
 export function courseRoutes(controller: CourseController): Router {
   const router = Router();
-  router.post("courses", (req, res) => controller.create(req, res));
-  router.put("courses/:id", (req, res) => controller.update(req, res));
-  router.get("courses", (req, res) => controller.list(req, res));
-  router.get("courses/:id", (req, res) => controller.findById(req, res));
-  router.delete("courses/:id", (req, res) => controller.remove(req, res));
+
+  router.post("courses", validateBody(CreateCourseDTO), controller.create);
+  router.put("courses/:id", validateParams(UpdateCourseDTO), controller.update);
+  router.get("courses", validateParams(ListCourseDTO), controller.list);
+  router.get("courses/:id", validateParams(CourseByIdDTO), controller.findById);
+  router.delete("courses/:id", validateParams(RemoveCourseDTO), controller.remove);
 
   return router;
 }
