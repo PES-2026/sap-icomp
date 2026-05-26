7dad174a6aa71b3f78780e8c50bc8bbd0fd57704
Nicolas <araujo.sampaio.nicolas@gmail.com>
2026-05-26 10:26:38 -0400

    feat: add update user functionality to UserController

--- backend/src/presentation/controllers/userController.ts
+++ backend/src/presentation/controllers/userController.ts
@@ -1,12 +1,17 @@
 import { Request, Response } from "express";
 
 import { ListUsersDTO } from "@application/dtos/user/listUsersDto";
+import { UpdateUserDTO } from "@application/dtos/user/updateUserDto";
 import { ListUsers } from "@application/useCases/user/listUsers";
+import { UpdateUser } from "@application/useCases/user/updateUser";
 
 import { BaseController } from "./baseController";
 
 export class UserController extends BaseController {
-  constructor(private readonly listUsersUseCase: ListUsers) {
+  constructor(
+    private readonly listUsersUseCase: ListUsers,
+    private readonly updateUserUseCase: UpdateUser,
+  ) {
     super();
   }
 
@@ -18,6 +23,17 @@ export class UserController extends BaseController {
       this.handleResult(res, result);
     } catch (error) {
       this.handleError(error, res, `${UserController.name}:list`);
+    }
+  };
+
+  update = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const dto = req.dto as UpdateUserDTO;
+      const result = await this.updateUserUseCase.execute(dto);
+
+      this.handleResult(res, result);
+    } catch (error) {
+      this.handleError(error, res, `${UserController.name}:update`);
     }
   };
 }
