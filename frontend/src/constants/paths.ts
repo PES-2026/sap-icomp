e05103b19e423694929223d25910d01ed53a4e95
davdprad <davidprado0113@gmail.com>
2026-05-31 23:18:11 -0400

    feat: professor home page

--- frontend/src/constants/paths.ts
+++ frontend/src/constants/paths.ts
@@ -1,8 +1,8 @@
 import { Role } from "@/features/login/types/login";
 
 export const RoleMap: { [K in Role as K]: Lowercase<K> } = {
-  Pedagogue: "pedagogue",
-  Professor: "professor",
+  PEDAGOGUE: "pedagogue",
+  PROFESSOR: "professor",
 } as const;
 
 export const PATHS = {
@@ -12,31 +12,31 @@ export const PATHS = {
   forgot_password: "/forgot-password",
 
   // Main Page Roles
-  pedagogue: `/${RoleMap.Pedagogue}`,
-  professor: `/${RoleMap.Professor}`,
+  pedagogue: `/${RoleMap.PEDAGOGUE}`,
+  professor: `/${RoleMap.PROFESSOR}`,
 
   // Scheduling
   scheduling: "/scheduling",
 
   // Students
-  students_list: `/${RoleMap.Pedagogue}/students`,
-  register_student: `/${RoleMap.Pedagogue}/students/register`,
+  students_list: `/${RoleMap.PEDAGOGUE}/students`,
+  register_student: `/${RoleMap.PEDAGOGUE}/students/register`,
   visualize_student: (enrollmentId: string) =>
-    `/${RoleMap.Pedagogue}/students/${enrollmentId}`,
+    `/${RoleMap.PEDAGOGUE}/students/${enrollmentId}`,
   edit_student: (enrollmentId: string) =>
-    `/${RoleMap.Pedagogue}/students/${enrollmentId}/edit`,
+    `/${RoleMap.PEDAGOGUE}/students/${enrollmentId}/edit`,
 
   // Attendances
-  attendances_list: `/${RoleMap.Pedagogue}/attendances`,
+  attendances_list: `/${RoleMap.PEDAGOGUE}/attendances`,
   register_attendance: (studentId: string) =>
-    `/${RoleMap.Pedagogue}/students/${studentId}/attendance/register`,
+    `/${RoleMap.PEDAGOGUE}/students/${studentId}/attendance/register`,
   visualize_attendance: (studentId: string, attendanceId: string) =>
-    `/${RoleMap.Pedagogue}/students/${studentId}/attendance/${attendanceId}`,
+    `/${RoleMap.PEDAGOGUE}/students/${studentId}/attendance/${attendanceId}`,
   edit_attendance: (studentId: string, attendanceId: string) =>
-    `/${RoleMap.Pedagogue}/students/${studentId}/attendance/${attendanceId}/edit`,
+    `/${RoleMap.PEDAGOGUE}/students/${studentId}/attendance/${attendanceId}/edit`,
 
   // Users
-  users_list: `/${RoleMap.Pedagogue}/users`,
+  users_list: `/${RoleMap.PEDAGOGUE}/users`,
 
-  visualize_settings: `/${RoleMap.Pedagogue}/settings`,
+  visualize_settings: `/${RoleMap.PEDAGOGUE}/settings`,
 };
