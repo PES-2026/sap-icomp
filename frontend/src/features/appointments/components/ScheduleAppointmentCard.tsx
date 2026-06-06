09db87f91f64030590b425177620ec0018e6f7a5
davdprad <davidprado0113@gmail.com>
2026-06-06 00:23:15 -0400

    feat: remove mock slots

--- frontend/src/features/appointments/components/ScheduleAppointmentCard.tsx
+++ frontend/src/features/appointments/components/ScheduleAppointmentCard.tsx
@@ -18,21 +18,11 @@ export default function ScheduleAppointmentCard() {
     control,
     handleSubmit,
     errors,
-    // slots,
     isLoadingSlots,
     isSubmitting,
     selectedSlotId,
     handleSelectSlot,
   } = useAppointmentForm();
-
-  const slots = [
-    { id: "1", time: "09:00", isAvailable: true },
-    { id: "2", time: "10:00", isAvailable: false },
-    { id: "3", time: "11:00", isAvailable: false },
-    { id: "4", time: "13:00", isAvailable: true },
-    { id: "5", time: "14:00", isAvailable: true },
-    { id: "6", time: "15:00", isAvailable: false },
-  ];
 
   const { coursesOptions } = useCoursesOptions();
   const { pedagogueOptions } = usePedagogueOptions();
